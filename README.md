15a875bc03e6653fd27975686dcd6129dacfe3d0
GYD102 <gyd102@gmail.com>
2016-02-26 23:29:36 -0500

    Added README and comments to encounterTest code for easier reading

---
+++ README.md
@@ -0,0 +1,38 @@
+Stizh Encounter Test
+====================
+
+Overview
+--------
+A program to calculate how much time players would have during a specifically
+designed encounter in 5th Edition Dungeons & Dragons®.
+
+Scenario
+--------
+A building is supported by 10 pillars around its periphery. These pillars are
+damaged by hidden traps which deal 11d8 (the sum of 11 rolls of an 8-sided die)
+damage to their cooresponding columns. Each column has a total of 50 hit points.
+Some of the traps (determined earlier) do not function properly. If a surviving
+pillar is neighbored by a demolished pillar, it takes a certain amount of damage
+per turn. Using a probabilistic approach, I can test various damage constants
+for the crumbling pillars and obtain the average number of turns until none of
+the pillars are left standing.
+
+Key
+---
+\* - Planned changes, modifications, improvements
+
+Usage
+-----
+1) Run the program encounterTest.py
+
+2) If it lags, create a KeyboardInterrupt to override the trial that is delaying
+   the process.
+   * This is caused by a trial in which none of the columns were demolished.
+
+3) The program will print an average number of turns until every column is
+   destroyed across a sample size of 1000 with damage constants of 3, 2.5, 2,
+   1.5, and 1 per demolished neighbor accordingly.
+
+Author
+------
+Glib Dolotov
--- encounterTest.py
+++ encounterTest.py
@@ -2,18 +2,27 @@ import math
 from random import randint
 from random import sample
 
+# Function sets all negative health point values to zero
 def flatten(x):
     return [i if (i >= 0) else 0 for i in x]
 
 def generateBase():
+# Create 10 columns with 50 hit points each
     S = [50 for i in range(10)]
+# Each column takes 11d8 damage
     for i in range(len(S)):
         S[i] = S[i] - sum([randint(1, 8) for j in range(11)])
+# Reset health for columns 3 and 5 (trap malfunction)
     S[2]= S[4] = 50
+# Reset health for column 8 and damage it for (3 * (11d8) / 4) damage
+# (trap malfucntion)
     S[7] = math.floor(50 - 3.0 * sum([randint(1, 8) for i in range(11)]) / 4.0)
+# Rid the hit point values of any negatives
     S = flatten(S)
     return S
 
+# Create damage coefficients by counting the number of neighboring columns that
+# are demolished (column 1 is neighbored by 10 and 2, 2 by 1 and 3, etc.)
 def neighbors(S, i):
     T = [S[(i + 1) % 10], S[i - 1]]
     count = 0
@@ -24,6 +33,8 @@ def neighbors(S, i):
             count = count - 0.5
     return count
 
+# Count how many turns it would take a randomly damaged set of columns to
+# collapse if each column takes dam damage from each demolished neighbor
 def countdown(S, dam):
     count = 0
     while (sum(S) > 0):
@@ -32,11 +43,15 @@ def countdown(S, dam):
             S = [S[i] + dam * neighbors(temp, i) for i in range(len(S))]
             S = flatten(S)
             count = count + 1
+# Handle the possibility of none of the columns having collapsed in which case
+# the columns would never collapse and the recursion would never end.
         except KeyboardInterrupt:
             print(S)
             return 0
     return count
         
+# Print the averages of 1000 trials for damage constants of 3, 2.5, 2, 1.5,
+# and 1 accordingly. Note that twice the damage constant is inputed.
 print(1.0 * sum([countdown(generateBase(),6) for i in range(1000)]) / 1000.0)
 print(1.0 * sum([countdown(generateBase(),5) for i in range(1000)]) / 1000.0)
 print(1.0 * sum([countdown(generateBase(),4) for i in range(1000)]) / 1000.0)
